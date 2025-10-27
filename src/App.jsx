c2de2765ae7f7fa7024ba31d429e76029085dae5
tmksh <miracle20230526@gmail.com>
2025-10-27 17:34:18 +0900

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter, useLocation } from "react-router-dom";
 import { Lamp } from "./screens/Lamp";
 import { Screen } from "./screens/Screen";
 import { Screen4 } from "./screens/Screen4";
@@ -7,34 +7,72 @@ import { ScreenScreen } from "./screens/ScreenScreen";
 import { ScreenWrapper } from "./screens/ScreenWrapper";
 import { Top } from "./screens/Top";
 
+const ScrollToTop = ({ children }) => {
+  const location = useLocation();
+
+  React.useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return children;
+};
+
 const router = createBrowserRouter([
   {
     path: "/*",
-    element: <Top />,
+    element: (
+      <ScrollToTop>
+        <Top />
+      </ScrollToTop>
+    ),
   },
   {
     path: "/topu12504u12442u12540u12471u12441",
-    element: <Top />,
+    element: (
+      <ScrollToTop>
+        <Top />
+      </ScrollToTop>
+    ),
   },
   {
     path: "/u32946u25104u12459u12522u12461u12517u12521u12512",
-    element: <Screen />,
+    element: (
+      <ScrollToTop>
+        <Screen />
+      </ScrollToTop>
+    ),
   },
   {
     path: "/u12424u12367u12354u12427u36074u21839",
-    element: <ScreenScreen />,
+    element: (
+      <ScrollToTop>
+        <ScreenScreen />
+      </ScrollToTop>
+    ),
   },
   {
     path: "/u21463u35611u12398u27969u12428",
-    element: <ScreenWrapper />,
+    element: (
+      <ScrollToTop>
+        <ScreenWrapper />
+      </ScrollToTop>
+    ),
   },
   {
     path: "/u35611u24107u32057u20171",
-    element: <Screen4 />,
+    element: (
+      <ScrollToTop>
+        <Screen4 />
+      </ScrollToTop>
+    ),
   },
   {
     path: "/lampu12398u32057u20171",
-    element: <Lamp />,
+    element: (
+      <ScrollToTop>
+        <Lamp />
+      </ScrollToTop>
+    ),
   },
 ]);
 
